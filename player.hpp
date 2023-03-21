efa4c13875509b491bbb29ecdc7360fafd5a4b07
FalseProgramemr <127978099+FalseProgramemr@users.noreply.github.com>
2023-03-21 16:16:51 +0100

    Update player.hpp

--- player.hpp
+++ player.hpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <thread>
 #include<ncurses.h>
 #include<unistd.h>
 
-
-
 class player {
     protected:
-        int xLoc, yLoc, xMax, yMax, dir, life, originx, originy;
+        int xLoc, yLoc, xMax, yMax, life, originx, originy, projx, projy, money;
+        bool s, dir, dirlock, j;
         char character;
         WINDOW * curwin;
 
@@ -22,7 +20,11 @@ class player {
         int move();
         void jump();
         int leftright();
-        void display();
+        void* display(void * arg);
         void stairsup();
         void stairsdown();
+        void lifeshow();
+        void gravity();
+        void shoot();
+        int playeroutput(int input);
 };
